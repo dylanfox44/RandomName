c885373cc4b694b5a57d1f61b007131b6913251b
dylanfox44 <131827295+dylanfox44@users.noreply.github.com>
2025-02-19 22:42:41 +0000

    random name in js only

---
+++ utilities.js
@@ -0,0 +1,8 @@
+function getRandomName(list) {
+    const randomIndex = Math.floor(Math.random() * list.length);
+    return list[randomIndex];
+}
+
+const names = ['Dylan', 'Gavin', 'Easton', 'Max', 'Jacob', 'Josh', 'Avery', 'Alex', 'Jack', 'Steve'];
+const randomName = getRandomName(names);
+console.log(randomName);
