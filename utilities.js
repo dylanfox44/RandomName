e871a33aa86ef3cdfb8d4da594016bad99aa2a24
dylanfox44 <131827295+dylanfox44@users.noreply.github.com>
2025-02-19 23:32:41 +0000

    displays name as a h1

--- utilities.js
+++ utilities.js
@@ -1,7 +1,5 @@
-function getRandomName(list) {
-    const randomIndex = Math.floor(Math.random() * list.length);
-    return list[randomIndex];
+function getRandomName() {
+    const names = ['Dylan', 'Gavin', 'Easton', 'Max', 'Jacob', 'Josh', 'Avery', 'Alex', 'Jack', 'Steve'];
+    return names[Math.floor(Math.random() * names.length)];
 }
 
-const names = ['Dylan', 'Gavin', 'Easton', 'Max', 'Jacob', 'Josh', 'Avery', 'Alex', 'Jack', 'Steve'];
-const randomName = getRandomName(names);
